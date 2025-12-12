ecaffad3b11ffbc94115b8f38cf34cfefd9a1e60
Dina sourani <dyxz8613@gmail.com>
2025-12-12 13:39:08 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static int Fib(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("یک عدد وارد کنید: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("سری فیبوناچی تا عدد {0} :", n);
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(Fib(i) + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
